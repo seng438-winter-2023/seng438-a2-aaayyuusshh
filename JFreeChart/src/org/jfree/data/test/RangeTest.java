c1ccd67218a8112a628f0920f479c8a87d3a74ae
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2023-02-08 19:22:30 -0700

    Add jar files, junit file path & gitignore

---
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -0,0 +1,26 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RangeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
